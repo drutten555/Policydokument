2c767139845a3cdd75c36986a1849ba74c4ce217
Wendy <wendy@chalmers.se>
2024-08-07 23:49:42 +0200

    removed unnecessary code

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from chromadb.utils.embedding_functions import OllamaEmbeddingFunction
 from openai import OpenAI
 
 
-def get_chatGPT_response(openai_client, query: str, context: List[str], model_name: str) -> str:
+def get_response(openai_client, query: str, context: List[str], model_name: str) -> str:
     """
     Queries the GPT API to get a response to the question.
 
@@ -23,23 +23,27 @@ def get_chatGPT_response(openai_client, query: str, context: List[str], model_na
     prompt = [
         {
             "role": "system",
-            "content": f"""I am going to ask you a question, which I would like you to answer.
-                First answer the question with yes or no, and then give an explaination based only on the provided context.
-                If there is not enough information in the context to answer the question, say "I am not sure", then try to make a guess.
-                Respond with a short and consice answer.
+            "content": f"""You are a helpful assistant that answers questions regarding Chalmers governing documents that will be provided.
+                Always answer the question with yes/no/unclear, and then provide an explanation and quotes based only on the documents.
+                If there is not enough information in the context to answer the question, then ask more questions. Do not assume anything about the user.
+                Respond with a short and concise answer.
+                Always provide a list of references containing the document's file name, 'Reference number/Diarienummer'.
             """
         },
         {
             "role": "user", 
-            "content": f"""I'm a Chalmers employee and the question is {query}. Here is all the context you have: {context}"""}
+            "content": f"""The question is {query}. Here is all the context you have: {context}"""}
     ]
 
     response = openai_client.chat.completions.create(
         model=model_name,
         messages=prompt,
+        temperature=0.7,
+        stream=True
     )
 
-    return response.choices[0].message.content  # type: ignore
+    # return response.choices[0].message.content  # type: ignore, return this if stream=False
+    return response
 
 
 def get_LLM():
@@ -47,21 +51,16 @@ def get_LLM():
     llm = input(
         "Pick an LLM model:\n" +
         "(1) OpenAI \n" +
-        "(2) Ollama\n\n" +
+        "(2) Ollama\n" +
         "=> Input: "
     )
     match int(llm):
-        case 1:
-            # OpenAI
-            model_name = "gpt-3.5-turbo"
-            answer = input(f"Do you want to use GPT-4o Mini? (y/n) (default is {model_name}): ")
-            if answer == "y":
-                model_name = "gpt-4o-mini"
+        case 1: # OpenAI
+            model_name = "gpt-4o-mini"
             if "OPENAI_API_KEY" not in os.environ:
                 os.environ["OPENAI_API_KEY"] = getpass.getpass()
             openai_client = OpenAI() # defaults to getting the key using os.environ.get("OPENAI_API_KEY")
-        case 2:
-            # Ollama
+        case 2: # Ollama
             model_name = "llama3.1"
             openai_client = OpenAI(
                 base_url = 'http://localhost:11434/v1',
@@ -70,6 +69,7 @@ def get_LLM():
         case _:
             print("\nYou didn\"t pick a valid option. Terminating...")
             exit()
+    print("=> Using", model_name)
     return model_name, openai_client
 
 
@@ -94,23 +94,27 @@ def main(
         )
     )
 
-    # We use a simple input loop.
     while True:
         # Get the user's query
-        query = input("=> Query: ")
+        query = input("\n=> Query: ")
         if len(query) == 0:
             print("Please enter a question. Ctrl+C to Quit.\n")
             continue
-        print(f"\n=> Thinking using {model_name}...\n")
 
         # Query the collection to get the 5 most relevant results
         results = collection.query(
-            query_texts=[query], n_results=5, include=["documents", "metadatas"]
+            query_texts=[query + "\nList the reference number/diarienummer for the documents."], 
+            n_results=5, 
+            include=["documents", "metadatas"]
         )
 
-        # Get the response from GPT
-        response = get_chatGPT_response(openai_client, query, results["documents"][0], model_name)  # type: ignore
-        print("Answer:", response)
+        # Get the response from LLM
+        response = get_response(openai_client, query, results["documents"][0], model_name)  # type: ignore
+        print("=> Answer:")
+        for chunk in response:
+            if chunk.choices[0].delta.content is not None:
+                print(chunk.choices[0].delta.content, end="", flush=True)
+        print("\n")
 
 
 if __name__ == "__main__":
